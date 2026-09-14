18e13e86b515a906f5305135e21e269d61b2b85b
ashw-n <ashwingudiseva@gmail.com>
2026-09-14 11:37:45 -0400

    Add intuitive terminate conditions

--- A2/TeamBuilder.java
+++ A2/TeamBuilder.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 
 public class TeamBuilder
 {
-    public static ArrayList<ArrayList<String>> buildTeams(Student[] students, 
-        int teamSize, double minTotalGPA, int[][] conflicts)
+    public static ArrayList<ArrayList<String>> buildTeams(Student[] students, int teamSize, double minTotalGPA, int[][] conflicts)
     {
         // Student implementation
 
+        //Terminate Early if: current size > max size, conflict detected, not enough students remaining, or if it cannot meet skill requirements
+        
         //return new ArrayList<ArrayList<String>>();
     }
 }
